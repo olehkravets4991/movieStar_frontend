4b167bd1e40cf56f12716e213f1b4ee3bbd146b4
Isaac Santacruz <Isaacsantacruz73@gmail.com>
2023-08-19 15:36:44 -0400

    update index and actions

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,16 +1,24 @@
-import { Link, useLoaderData, Form } from "react-router-dom";
+import { Link, useLoaderData, Form, useNavigate} from "react-router-dom";
+import { baseUrl } from "../base_url";
+
 
 function Index(props) {
   // GET THE DATA FROM OUR LOADER
   const movie = useLoaderData();
-
+  const navigate = useNavigate() // get function to send people to other pages
   return (
     <div>
+         <button onClick={async () => {
+        await fetch(`${baseUrl}/logout`) // make a request to the server to logout
+        localStorage.removeItem('loggedIn') // remove the token from local storage
+        navigate("/") // send the user back to the home page
+      }}>Logout</button>
+      
         <h2>Create a new movie </h2>
         <Form action="/create" method="post">
             <input type="text" name="Title" placeholder="Movie's title"/>
             <input type="text" name="Genres" placeholder="Movie's Genres"/>
-            <input type="text" name="Release date" placeholder="Movie's Release date"/>
+            <input type="text" name="ReleaseDate" placeholder="Movie's Release date"/>
             <input type="text" name="Poster" placeholder="Movie's Poster"/>
             <input type="submit" value="Create a movie"/>
         </Form>
